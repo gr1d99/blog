27cf04a1b17065a618434187f9a8bf3330529495
gr1d99 <gideonkimutai9@gmail.com>
2017-07-31 16:25:00 +0300

    updated required_apps function

--- blog_settings/settings.py
+++ blog_settings/settings.py
@@ -1,5 +1,10 @@
-INSTALLED_APPS = []
+from django.conf import settings
+INSTALLED_APPS = [
+    'sms_simple',
+]
+
 
 def get_required_apps():
-    return INSTALLED_APPS
+    # ensure that there are no duplicate apps
+    return [app for app in INSTALLED_APPS if app not in settings.INSTALLED_APPS]
 
